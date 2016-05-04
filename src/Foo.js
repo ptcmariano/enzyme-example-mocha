8ba33ef348bebc853fd850a2e6002917dfb18342
Paulo Mariano <paulo.mariano@catho.com>
2016-05-04 11:26:50 -0300

    add case with element having children

--- src/Foo.js
+++ src/Foo.js
@@ -11,7 +11,9 @@ class Foo extends React.Component {
 
   render() {
     return (
-      <div className="foo" />
+      <div className="foo">
+        <div className="bar" />
+      </div>
     );
   }
 }
--- test/Foo-test.js
+++ test/Foo-test.js
@@ -3,16 +3,20 @@ import { expect } from 'chai';
 import { shallow, mount, render } from 'enzyme';
 import Foo from '../src/Foo';
 
-describe("A suite", function() {
-  it("contains spec with an expectation", function() {
-    expect(shallow(<Foo />).contains(<div className="foo" />)).to.equal(true);
+describe("Define element Foo", function() {
+  it("should contains element div.bar", function() {
+    expect(shallow(<Foo />).contains(<div className="bar" />)).to.equal(true);
   });
 
-  it("contains spec with an expectation", function() {
-    expect(shallow(<Foo />).is('.foo')).to.equal(true);
-  });
-
-  it("contains spec with an expectation", function() {
+  it("contains className .foo count one", function() {
     expect(mount(<Foo />).find('.foo').length).to.equal(1);
+  });
+
+  it("should prop className equal foo", function() {
+    expect(shallow(<Foo />).props().className).to.equal("foo");
+  });
+
+  it("should prop className of children equal bar", function() {
+    expect(shallow(<Foo />).children().prop("className")).to.equal("bar");
   });
 });
